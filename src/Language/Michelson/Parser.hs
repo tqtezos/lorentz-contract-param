8503a8a8bc8e1b77c7d8ed0cc1767c24f12ebce9
John C. Burnham <jcb@johnchandlerburnham.com>
2018-11-07 00:17:14 -0500

    morley now parses datatypes

--- src/Language/Michelson/Parser.hs
+++ src/Language/Michelson/Parser.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 module Language.Michelson.Parser where
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base16 as B16
+import           Data.Sequence
+import           Data.Char as Char
 import qualified Data.Text                        as T
+import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Text.IO                     as TIO
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
@@ -10,7 +14,8 @@ import qualified Text.Megaparsec.Char.Lexer       as L
 
 import qualified Language.Michelson.Types         as M
 
-import Control.Applicative.Permutations
+import Data.Natural
+import           Control.Applicative.Permutations
 import qualified Data.Sequence                    as Seq
 import           Data.Void                        (Void)
 
@@ -36,9 +41,9 @@ parens = between (symbol "(") (symbol ")")
 braces = between (symbol "{") (symbol "}")
 sem = symbol ";"
 
-
 smart_contract :: Parser M.SC
 smart_contract = do
+  m_space
   (p,s,c) <- runPermutation $
               (,,) <$> toPermutation param
                    <*> toPermutation storage
@@ -46,182 +51,169 @@ smart_contract = do
   return $ M.SC p s c
 
 param :: Parser M.Param
-param = (symbol "parameter") >> m_type >>= (\a -> sem >> (return $ M.Param a))
+param = do symbol "parameter"; a <- m_type; sem; return $ M.Param a
 
 storage :: Parser M.Storage
-storage = (symbol "storage") >> m_type >>= (\a -> sem >> (return $ M.Storage a))
+storage = do symbol "storage"; a <- m_type; sem; return $ M.Storage a
 
 code :: Parser M.Code
-code = (symbol "code") >> iseq >>= (\a -> sem >> (return $ M.Code a))
+code = do symbol "code"; a <- iseq; sem; return $ M.Code a
 
 -- Data
 m_data :: Parser M.D
-m_data = lexeme $
-  d_Unit <|> d_True <|> d_False
+m_data = lexeme $ (m_data' <|> parens m_data')
 
--- todo: natural numbers, signed numbers
+m_data' :: Parser M.D
+m_data' = (try $ M.LInt <$> l_int)
+      <|> (try $ M.LString <$> l_string)
+      <|> (try $ M.LBytes <$> l_bytes)
+      <|> (do symbol "Unit"; return M.DUnit)
+      <|> (do symbol "True"; return M.DTrue)
+      <|> (do symbol "False"; return M.DFalse)
+      <|> (do symbol "Pair"; a <- m_data; b <- m_data; return $ M.DPair a b)
+      <|> (do symbol "Left"; a <- m_data; return $ M.DLeft a)
+      <|> (do symbol "Right"; a <- m_data; return $ M.DRight a)
+      <|> (do symbol "Some"; a <- m_data; return $ M.DSome a)
+      <|> (do symbol "None"; return M.DNone)
+      <|> (try $ M.DSeq <$> d_list)
+      <|> (try $ M.DMap <$> d_map)
+      <|> (M.DInst <$> iseq)
 
 l_int :: Parser Integer
-l_int = L.decimal <|> (string "0x" >> L.hexadecimal)
--- todo: escape sequences
+l_int = (L.signed (return ()) L.decimal)
 
---strChar :: Parser Char
--- strChar = oneOf [' '...'
+l_bytes :: Parser B.ByteString
+l_bytes = do
+  symbol "0x"
+  hexdigits <- takeWhile1P Nothing Char.isHexDigit
+  let (bytes, remain) = B16.decode $ encodeUtf8 hexdigits
+  if remain == ""
+  then return bytes
+  else (error "odd number bytes")
 
--- escapeSeqs :: Parser Char
--- escapeSeqs =
-
+-- this parses more escape sequences than are in the michelson spec
+-- should investigate which sequences this matters for, e.g. \xa == \n
 l_string :: Parser T.Text
-l_string = do
-  char '"'
-  str <- manyTill L.charLiteral (char '"')
-  return $ T.pack str
+l_string = T.pack <$> (char '"' >> (manyTill L.charLiteral (char '"')))
 
--- todo: various "string" literals
+{-
+-- could do something explicit based on this
+strEscape :: Parser T.Text
+strEscape = char '\\' >> esc
+  where
+    esc = (char 'n' >> return "\n")
+      <|> (char 't' >> return "\t")
+      <|> (char 'b' >> return "\b")
+      <|> (char '\\' >> return "\\")
+      <|> (char '"' >> return "\"")
+-}
 
-d_Unit :: Parser M.D
-d_Unit = string "Unit" >> return M.DUnit
 
-d_True :: Parser M.D
-d_True = string "True" >> return M.DTrue
+d_list :: Parser (Seq M.D)
+d_list = Seq.fromList <$> (braces $ sepEndBy m_data sem)
 
-d_False :: Parser M.D
-d_False = string "False" >> return M.DFalse
+d_elt :: Parser M.Elt
+d_elt = do symbol "Elt"; key <- m_data; val <- m_data; return $ M.Elt key val
 
-d_Pair :: Parser M.D
-d_Pair = do
-  string "Pair"
-  a <- m_data
-  b <- m_data
-  return $ M.DPair a b
-
-d_Left :: Parser M.D
-d_Left = do
-  string "Left"
-  a <- m_data
-  return $ M.DLeft a
-
-d_Right :: Parser M.D
-d_Right = do
-  string "Right"
-  a <- m_data
-  return $ M.DRight a
-
-d_Some :: Parser M.D
-d_Some = do
-  string "Some"
-  a <- m_data
-  return $ M.DSome a
-
-d_None :: Parser M.D
-d_None = string "None" >> return M.DNone
+d_map :: Parser (Seq M.Elt)
+d_map = Seq.fromList <$> (braces $ sepEndBy d_elt sem)
 
 -- Instructions
-
 iseq :: Parser M.ISeq
-iseq = lexeme $ braces $ M.ISeq . Seq.fromList <$> many instruction
+iseq = M.ISeq . Seq.fromList <$> (braces $ sepEndBy instruction sem)
 
 instruction :: Parser M.I
-instruction = ins <* sem where
-  ins = (symbol "DROP" >> return M.DROP)
-    <|> (symbol "DUP" >> return M.DUP)
-    <|> (symbol "SWAP" >> return M.SWAP)
-    <|> (symbol "PUSH" >> m_type >>= (\a -> m_data >>= (\b ->
-          return $ M.PUSH a b)))
-    <|> (string "SOME" >> return M.SOME)
-    <|> (symbol "NONE" >> m_type >>= (\a -> return $ M.NONE a))
-    <|> (symbol "UNIT" >> return M.UNIT)
-    <|> (symbol "IF_NONE" >> iseq >>= (\a -> iseq >>= (\b ->
-          return $ M.IF_NONE a b)))
-    <|> (symbol "PAIR" >> return M.PAIR)
-    <|> (symbol "CAR" >> return M.CAR)
-    <|> (symbol "CDR" >> return M.CDR)
-    <|> (symbol "LEFT" >> m_type >>= (\a -> return $ M.LEFT a))
-    <|> (symbol "RIGHT" >> m_type >>= (\a -> return $ M.RIGHT a))
-    <|> (symbol "NIL" >> m_type >>= (\a -> return $ M.NIL a))
-    <|> (symbol "CONS" >> return M.CONS)
-    <|> (symbol "IF_CONS" >> iseq >>= (\a -> iseq >>= (\b ->
-          return $ M.IF_CONS a b)))
-    <|> (symbol "EMPTY_SET" >> m_cmptype >>= (\a -> return $ M.EMPTY_SET a))
-    <|> (symbol "EMPTY_MAP" >> m_cmptype >>= (\a -> m_type >>= (\b ->
-          return $ M.EMPTY_SET a)))
-    <|> (symbol "MAP" >> iseq >>= \a -> return $ M.MAP a)
-    <|> (symbol "ITER" >> iseq >>= \a -> return $ M.ITER a)
-    <|> (symbol "MEM" >> return M.MEM)
-    <|> (symbol "GET" >> return M.GET)
-    <|> (symbol "UPDATE" >> return M.UPDATE)
-    <|> (symbol "IF" >> iseq >>= (\a -> iseq >>= (\b ->
-          return $ M.IF a b)))
-    <|> (symbol "LOOP" >> iseq >>= \a -> return $ M.LOOP a)
-    <|> (symbol "LOOP_LEFT" >> iseq >>= \a -> return $ M.LOOP_LEFT a)
-    <|> (symbol "EXEC" >> return M.EXEC)
-    <|> (symbol "DIP" >> iseq >>= \a -> return $ M.DIP a)
-    <|> (symbol "FAILWITH" >> m_data >>= (\a -> return $ M.FAILWITH a))
-    <|> (symbol "CAST" >> return M.CAST)
-    <|> (symbol "RENAME" >> return M.RENAME)
-    <|> (symbol "CONCAT" >> return M.CONCAT)
-    <|> (symbol "ADD" >> return M.ADD)
-    <|> (symbol "SUB" >> return M.SUB)
-    <|> (symbol "MUL" >> return M.MUL)
-    <|> (symbol "DIV" >> return M.DIV)
-    <|> (symbol "ABS" >> return M.ABS)
-    <|> (symbol "NEG" >> return M.NEG)
-    <|> (symbol "MOD" >> return M.MOD)
-    <|> (symbol "LSL" >> return M.LSL)
-    <|> (symbol "LSR" >> return M.LSR)
-    <|> (symbol "OR" >> return M.OR)
-    <|> (symbol "AND" >> return M.AND)
-    <|> (symbol "NOT" >> return M.NOT)
-    <|> (symbol "COMPARE" >> return M.COMPARE)
-    <|> (symbol "EQ" >> return M.EQ)
-    <|> (symbol "NEQ" >> return M.NEQ)
-    <|> (symbol "LT" >> return M.LT)
-    <|> (symbol "GT" >> return M.GT)
-    <|> (symbol "LE" >> return M.LE)
-    <|> (symbol "GE" >> return M.GE)
-    <|> (symbol "INT" >> return M.INT)
-    <|> (symbol "SELF" >> return M.SELF)
-    <|> (symbol "TRANSFER_TOKENS" >> return M.TRANSFER_TOKENS)
-    <|> (symbol "SET_DELEGATE" >> return M.SET_DELEGATE)
-    <|> (symbol "CREATE_ACCOUNT" >> return M.CREATE_ACCOUNT)
-    <|> (symbol "CREATE_CONTRACT" >> iseq >>= (\a ->
-          return $ M.CREATE_CONTRACT2 a))
-    <|> (symbol "CREATE_CONTRACT" >> return M.CREATE_CONTRACT)
-    <|> (symbol "IMPLICIT_ACCOUNT" >> return M.IMPLICIT_ACCOUNT)
-    <|> (symbol "NOW" >> return M.NOW)
-    <|> (symbol "AMOUNT" >> return M.AMOUNT)
-    <|> (symbol "BALANCE" >> return M.BALANCE)
-    <|> (symbol "CHECK_SIGNATURE" >> return M.CHECK_SIGNATURE)
-    <|> (symbol "BLAKE2B" >> return M.BLAKE2B)
-    <|> (symbol "HASH_KEY" >> return M.HASH_KEY)
-    <|> (symbol "STEPS_TO_QUOTA" >> return M.STEPS_TO_QUOTA)
-    <|> (symbol "SOURCE" >> return M.SOURCE)
-    <|> (symbol "SENDER" >> return M.SENDER)
+instruction = (symbol "DROP" >> return M.DROP)
+    <|> (do symbol "DUP"; return M.DUP)
+    <|> (do symbol "SWAP"; return M.SWAP)
+    <|> (do symbol "PUSH"; a <- m_type; b <- m_data; return $ M.PUSH a b)
+    <|> (do symbol "SOME"; return M.SOME)
+    <|> (do symbol "NONE"; a <- m_type; return $ M.NONE a)
+    <|> (do symbol "UNIT"; return M.UNIT)
+    <|> (do symbol "IF_NONE"; a <- iseq; b <- iseq; return $ M.IF_NONE a b)
+    <|> (do symbol "PAIR"; return M.PAIR)
+    <|> (do symbol "CAR"; return M.CAR)
+    <|> (do symbol "CDR"; return M.CDR)
+    <|> (do symbol "LEFT"; a <- m_type; return $ M.LEFT a)
+    <|> (do symbol "RIGHT"; a <- m_type; return $ M.RIGHT a)
+    <|> (do symbol "NIL"; a <- m_type; return $ M.NIL a)
+    <|> (do symbol "CONS"; return M.CONS)
+    <|> (do symbol "IF_CONS"; a <- iseq; b <- iseq; return $ M.IF_CONS a b)
+    <|> (do symbol "EMPTY_SET"; a <- m_ct; return $ M.EMPTY_SET a)
+    <|> (do symbol "EMPTY_MAP"; a <- m_ct; b <- m_type; return $ M.EMPTY_SET a)
+    <|> (do symbol "MAP"; a <- iseq; return $ M.MAP a)
+    <|> (do symbol "ITER"; a <- iseq; return $ M.ITER a)
+    <|> (do symbol "MEM"; return M.MEM)
+    <|> (do symbol "GET"; return M.GET)
+    <|> (do symbol "UPDATE"; return M.UPDATE)
+    <|> (do symbol "IF"; a <- iseq; b <- iseq; return $ M.IF a b)
+    <|> (do symbol "LOOP"; a <- iseq; return $ M.LOOP a)
+    <|> (do symbol "LOOP_LEFT"; a <- iseq; return $ M.LOOP_LEFT a)
+    <|> (do symbol "EXEC"; return M.EXEC)
+    <|> (do symbol "DIP"; a <- iseq; return $ M.DIP a)
+    <|> (do symbol "FAILWITH"; a <- m_data; return $ M.FAILWITH a)
+    <|> (do symbol "CAST"; return M.CAST)
+    <|> (do symbol "RENAME"; return M.RENAME)
+    <|> (do symbol "CONCAT"; return M.CONCAT)
+    <|> (do symbol "ADD"; return M.ADD)
+    <|> (do symbol "SUB"; return M.SUB)
+    <|> (do symbol "MUL"; return M.MUL)
+    <|> (do symbol "DIV"; return M.DIV)
+    <|> (do symbol "ABS"; return M.ABS)
+    <|> (do symbol "NEG"; return M.NEG)
+    <|> (do symbol "MOD"; return M.MOD)
+    <|> (do symbol "LSL"; return M.LSL)
+    <|> (do symbol "LSR"; return M.LSR)
+    <|> (do symbol "OR"; return M.OR)
+    <|> (do symbol "AND"; return M.AND)
+    <|> (do symbol "NOT"; return M.NOT)
+    <|> (do symbol "COMPARE"; return M.COMPARE)
+    <|> (do symbol "EQ"; return M.EQ)
+    <|> (do symbol "NEQ"; return M.NEQ)
+    <|> (do symbol "LT"; return M.LT)
+    <|> (do symbol "GT"; return M.GT)
+    <|> (do symbol "LE"; return M.LE)
+    <|> (do symbol "GE"; return M.GE)
+    <|> (do symbol "INT"; return M.INT)
+    <|> (do symbol "SELF"; return M.SELF)
+    <|> (do symbol "TRANSFER_TOKENS"; return M.TRANSFER_TOKENS)
+    <|> (do symbol "SET_DELEGATE"; return M.SET_DELEGATE)
+    <|> (do symbol "CREATE_ACCOUNT"; return M.CREATE_ACCOUNT)
+    <|> (do symbol "CREATE_CONTRACT"; a <- iseq; return $ M.CREATE_CONTRACT2 a)
+    <|> (do symbol "CREATE_CONTRACT"; return M.CREATE_CONTRACT)
+    <|> (do symbol "IMPLICIT_ACCOUNT"; return M.IMPLICIT_ACCOUNT)
+    <|> (do symbol "NOW"; return M.NOW)
+    <|> (do symbol "AMOUNT"; return M.AMOUNT)
+    <|> (do symbol "BALANCE"; return M.BALANCE)
+    <|> (do symbol "CHECK_SIGNATURE"; return M.CHECK_SIGNATURE)
+    <|> (do symbol "BLAKE2B"; return M.BLAKE2B)
+    <|> (do symbol "HASH_KEY"; return M.HASH_KEY)
+    <|> (do symbol "STEPS_TO_QUOTA"; return M.STEPS_TO_QUOTA)
+    <|> (do symbol "SOURCE"; return M.SOURCE)
+    <|> (do symbol "SENDER"; return M.SENDER)
+    <|> (do symbol "ADDRESS"; return M.ADDRESS)
 
 -- Types
 m_type :: Parser M.T
-m_type = lexeme $ (symbol "key" >> return M.T_key)
-  <|> (symbol "unit" >> return M.T_unit)
-  <|> (symbol "signature" >> return M.T_signature)
-  <|> (symbol "option" >> m_type >>= (\a -> return $ M.T_option a))
-  <|> (symbol "list" >> m_type >>= (\a -> return $ M.T_list a))
-  <|> (symbol "set" >> m_cmptype >>= (\a -> return $ M.T_set a))
-  <|> (symbol "contract" >> m_type >>= (\a -> return $ M.T_contract a))
-  <|> (symbol "pair" >> m_type >>= (\a -> m_type >>= (\b ->
-        return $ M.T_pair a b)))
-  <|> (symbol "or" >> m_type >>= (\a -> m_type >>= (\b ->
-        return $ M.T_or a b)))
-  <|> (symbol "lambda" >> m_type >>= (\a -> m_type >>= (\b ->
-        return $ M.T_lambda a b)))
-  <|> (symbol "map" >> m_cmptype >>= (\a -> m_type >>= (\b ->
-        return $ M.T_map a b)))
-  <|> (symbol "big_map" >> m_cmptype >>= (\a -> m_type >>= (\b ->
-        return $ M.T_big_map a b)))
-  <|> parens m_type
+m_type = (M.T_comparable <$> m_ct)
+  <|> (do symbol "key"; return M.T_key)
+  <|> (do symbol "unit"; return M.T_unit)
+  <|> (do symbol "signature"; return M.T_signature)
+  <|> (do symbol "option"; a <- m_type; return $ M.T_option a)
+  <|> (do symbol "list"; a <- m_type; return $ M.T_list a)
+  <|> (do symbol "set"; a <- m_ct; return $ M.T_set a)
+  <|> (do symbol "operation"; return $ M.T_operation)
+  <|> (do symbol "contract"; a <- m_type; return $ M.T_contract a)
+  <|> (do symbol "pair"; a <- m_type; b <- m_type; return $ M.T_pair a b)
+  <|> (do symbol "or"; a <- m_type; b <- m_type; return $ M.T_or a b)
+  <|> (do symbol "lambda"; a <- m_type; b <- m_type; return $ M.T_lambda a b)
+  <|> (do symbol "map"; a <- m_ct; b <- m_type; return $ M.T_map a b)
+  <|> (do symbol "big_map"; a <- m_ct; b <- m_type; return $ M.T_big_map a b)
+  <|> (parens m_type)
 
 -- Comparable Types
-m_cmptype :: Parser M.CT
-m_cmptype = lexeme $ (symbol "int" >> return M.T_int)
+m_ct :: Parser M.CT
+m_ct = (symbol "int" >> return M.T_int)
  <|> (symbol "nat" >> return M.T_nat)
  <|> (symbol "string" >> return M.T_string)
  <|> (symbol "mutez" >> return M.T_mutez)
