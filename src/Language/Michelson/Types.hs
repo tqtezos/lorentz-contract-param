c540a1e07e710f358fcee4124ec4108a1cd2f93f
John C. Burnham <jcb@johnchandlerburnham.com>
2018-12-29 09:50:50 -0500

    a big refactor and cleanup of the types and parsing

--- src/Language/Michelson/Types.hs
+++ src/Language/Michelson/Types.hs
@@ -1,31 +1,27 @@
 {-# LANGUAGE GADTs             #-}
-{-# LANGUAGE NoImplicitPrelude             #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Language.Michelson.Types where
 
-import           Data.Natural
+import qualified Data.ByteString as B
 import           Data.Maybe
-import           Data.Sequence as Seq
-import qualified Data.Text     as T
-import qualified Data.ByteString     as B
-import Text.Show
-import Prelude (Integer, (.), (++), Eq, Ord)
+import           Data.Natural
+import           Data.Sequence   as Seq
+import qualified Data.Text       as T
+import           Prelude         (Eq, Integer, Ord, (++), (.))
+import           Text.Show
 
 
--- smart contract
+{- Contract types -}
 data Contract = Contract Parameter Storage Code deriving Show
 
--- parameter
 data Parameter = Parameter Type deriving Show
-
 data Storage = Storage Type deriving Show
-
 data Code = Code Ops deriving Show
 
--- element of a map
-data Element = Element Data Data deriving Show
+data Stack = Stack {elems :: Seq Data} deriving Show
 
--- data
+{- Data types -}
 data Data where
   Int        :: Integer -> Data
   String     :: T.Text -> Data
@@ -39,223 +35,174 @@ data Data where
   Some       :: Data -> Data
   None       :: Data
   Seq        :: Seq Data -> Data
-  Map        :: Seq Element -> Data
+  Map        :: Seq Elt -> Data
   DataOps    :: Ops -> Data
+  deriving Show
+
+data Elt = Elt Data Data deriving Show
+
+{- Michelson Types -}
+
+-- Type Annotations
+type TypeNote = Maybe T.Text
+type FieldNote = Maybe T.Text
+type VarNote = Maybe T.Text
+
+-- Annotated type
+data Type = Type T TypeNote FieldNote deriving Show
+
+-- Annotated Comparable Sub-type
+data Comparable = Comparable CT TypeNote deriving Show
+
+-- Michelson Type
+data T =
+    T_comparable CT
+  | T_key
+  | T_unit
+  | T_signature
+  | T_option Type
+  | T_list Type
+  | T_set Comparable
+  | T_operation
+  | T_contract Type
+  | T_pair Type Type
+  | T_or Type Type
+  | T_lambda Type Type
+  | T_map Comparable Type
+  | T_big_map Comparable Type
+  deriving Show
+
+-- Comparable Sub-Type
+data CT =
+    T_int
+  | T_nat
+  | T_string
+  | T_bytes
+  | T_mutez
+  | T_bool
+  | T_key_hash
+  | T_timestamp
+  | T_address
   deriving Show
 
 -- instruction sequence
 data Ops = Ops { ops :: Seq Op } deriving Show
+data Prims = Prims { instrs :: Seq I } deriving Show
 
-opsConcat :: Ops -> Ops -> Ops
-opsConcat x y = Ops ((ops x) Seq.>< (ops y))
-
-(><) :: Ops -> Ops -> Ops
-infixr 9 ><
-(><) = opsConcat
-
-opsLappend :: Op -> Ops -> Ops
-opsLappend x y = Ops (x Seq.<| (ops y))
-
-(<|) :: Op -> Ops -> Ops
-infixr 9 <|
-(<|) = opsLappend
-
-opsRappend :: Ops -> Op -> Ops
-opsRappend x y = Ops ((ops x) Seq.|> y)
-
-(|>) :: Ops -> Op -> Ops
-infixr 9 |>
-(|>) = opsRappend
-
-noOps :: Ops
-noOps = Ops Seq.empty
-
-opsFromList :: [Op] -> Ops
-opsFromList = Ops . Seq.fromList
-
-(|:) :: [Op] -> Ops
-infixr 9 |:
-(|:) = opsFromList
-
-opsSingleton :: Op -> Ops
-opsSingleton x = opsFromList [x]
+{- Michelson Instructions and Instruction Macros -}
 
 data PairStruct = Leaf | Nest PairStruct PairStruct deriving Show
 data CadrStruct = A | D deriving Show
 
--- instruction
-data Op where
-  CMP_MAC           :: Op -> Op
-  IF_MAC            :: Op -> Ops -> Ops -> Op
-  IFCMP_MAC         :: Op -> Ops -> Ops ->Op
-  FAIL_MAC          :: Op
-  PAIR_MAC          :: PairStruct -> VarNote -> [FieldNote] -> Op
-  UNPAIR_MAC        :: PairStruct -> [VarNote] -> [FieldNote] -> Op
-  CADR_MAC          :: [CadrStruct] -> VarNote -> FieldNote -> Op
-  SET_CADR_MAC      :: [CadrStruct] -> VarNote -> FieldNote -> Op
-  MAP_CADR_MAC      :: [CadrStruct] -> VarNote -> FieldNote -> Ops -> Op
-  DIP_MAC           :: Integer -> Ops -> Op
-  DUP_MAC           :: Integer -> VarNote -> Op
-  ASSERT_MAC        :: Op
-  ASSERTX_MAC       :: Op -> Op
-  ASSERT_CMP_MAC    :: Op -> Op
-  ASSERT_NONE_MAC   :: Op
-  ASSERT_SOME_MAC   :: Op
-  ASSERT_LEFT_MAC   :: Op
-  ASSERT_RIGHT_MAC  :: Op
-  IF_SOME_MAC       :: Ops -> Ops -> Op
-  OpsSeq            :: Ops -> Op
-  DROP              :: Op
-  DUP               :: VarNote -> Op
-  SWAP              :: Op
-  PUSH              :: VarNote -> Type -> Data -> Op
-  SOME              :: TypeNote -> VarNote -> FieldNote
-                       -> Op
-  NONE              :: TypeNote -> VarNote -> FieldNote
-                       -> Type -> Op
-  UNIT              :: TypeNote -> VarNote -> Op
-  IF_NONE           :: Ops -> Ops -> Op
-  PAIR              :: TypeNote -> VarNote -> FieldNote -> FieldNote
-                       -> Op
-  CAR               :: VarNote -> FieldNote -> Op
-  CDR               :: VarNote -> FieldNote -> Op
-  LEFT              :: TypeNote -> VarNote -> FieldNote -> FieldNote
-                       -> Type -> Op
-  RIGHT             :: TypeNote -> VarNote -> FieldNote -> FieldNote
-                       -> Type -> Op
-  IF_LEFT           :: Ops -> Ops -> Op
-  IF_RIGHT          :: Ops -> Ops -> Op
-  NIL               :: TypeNote -> VarNote -> Type -> Op
-  CONS              :: VarNote -> Op
-  IF_CONS           :: Ops -> Ops -> Op
-  SIZE              :: VarNote -> Op
-  EMPTY_SET         :: TypeNote -> VarNote -> Comparable -> Op
-  EMPTY_MAP         :: TypeNote -> VarNote
-                       -> Comparable -> Type -> Op
-  MAP               :: VarNote -> Ops -> Op
-  ITER              :: VarNote -> Ops -> Op
-  MEM               :: VarNote -> Op
-  GET               :: VarNote -> Op
-  UPDATE            :: Op
-  IF                :: Ops -> Ops -> Op
-  LOOP              :: Ops -> Op
-  LOOP_LEFT         :: Ops -> Op
-  LAMBDA            :: VarNote -> Type -> Type -> Ops -> Op
-  EXEC              :: VarNote -> Op
-  DIP               :: Ops -> Op
-  FAILWITH          :: Op
-  CAST              :: TypeNote -> VarNote -> Op
-  RENAME            :: VarNote -> Op
-  PACK              :: VarNote -> Op
-  UNPACK            :: VarNote -> Type -> Op
-  CONCAT            :: VarNote -> Op
-  SLICE             :: VarNote -> Op
-  ISNAT             :: Op
-  ADD               :: VarNote -> Op
-  SUB               :: VarNote -> Op
-  MUL               :: VarNote -> Op
-  EDIV              :: VarNote -> Op
-  ABS               :: VarNote -> Op
-  NEG               :: Op
-  MOD               :: Op
-  LSL               :: VarNote -> Op
-  LSR               :: VarNote -> Op
-  OR                :: VarNote -> Op
-  AND               :: VarNote -> Op
-  XOR               :: VarNote -> Op
-  NOT               :: VarNote -> Op
-  COMPARE           :: VarNote -> Op
-  EQ                :: VarNote -> Op
-  NEQ               :: VarNote -> Op
-  LT                :: VarNote -> Op
-  GT                :: VarNote -> Op
-  LE                :: VarNote -> Op
-  GE                :: VarNote -> Op
-  INT               :: VarNote -> Op
-  SELF              :: VarNote -> Op
-  CONTRACT          :: Type -> Op
-  TRANSFER_TOKENS   :: VarNote -> Op
-  SET_DELEGATE      :: Op
-  CREATE_ACCOUNT    :: VarNote -> VarNote -> Op
-  CREATE_CONTRACT   :: VarNote -> VarNote -> Op
-  CREATE_CONTRACT2  :: VarNote -> VarNote -> Contract -> Op
-  IMPLICIT_ACCOUNT  :: VarNote -> Op
-  NOW               :: VarNote -> Op
-  AMOUNT            :: VarNote -> Op
-  BALANCE           :: VarNote -> Op
-  CHECK_SIGNATURE   :: VarNote -> Op
-  SHA256            :: VarNote -> Op
-  SHA512            :: VarNote -> Op
-  BLAKE2B           :: VarNote -> Op
-  HASH_KEY          :: VarNote -> Op
-  STEPS_TO_QUOTA    :: VarNote -> Op
-  SOURCE            :: VarNote -> Op
-  SENDER            :: VarNote -> Op
-  ADDRESS           :: VarNote -> Op
+data Macro =
+    CMP I
+  | IFX I Ops Ops
+  | IFCMP I Ops Ops
+  | FAIL
+  | PAPAIR PairStruct VarNote [FieldNote]
+  | UNPAIR PairStruct [VarNote] [FieldNote]
+  | CADR [CadrStruct] VarNote FieldNote
+  | SET_CADR [CadrStruct] VarNote FieldNote
+  | MAP_CADR [CadrStruct] VarNote FieldNote Ops
+  | DIIP Integer Ops
+  | DUUP Integer VarNote
+  | ASSERT
+  | ASSERTX I
+  | ASSERT_CMP I
+  | ASSERT_NONE
+  | ASSERT_SOME
+  | ASSERT_LEFT
+  | ASSERT_RIGHT
+  | IF_SOME Ops Ops
   deriving Show
 
--- type
-
-data TypeNote = TypeNote (Maybe T.Text) deriving (Show, Eq, Ord)
-data FieldNote = FieldNote (Maybe T.Text) deriving (Show, Eq, Ord)
-data VarNote = VarNote (Maybe T.Text) deriving (Show, Eq, Ord)
-
-data Type = Type T TypeNote FieldNote deriving Show
-
---instance Show Type where
---  show (Type (T_comparable ct) tn fn) = (show ct) ++ (show tn) ++ (show fn)
---  show (Type t tn fn) = (show t) ++ (show tn) ++ (show fn)
-
-data T where
-  T_comparable :: CT -> T
-  T_key        :: T
-  T_unit       :: T
-  T_signature  :: T
-  T_option     :: Type -> T
-  T_list       :: Type -> T
-  T_set        :: Comparable -> T
-  T_operation  :: T
-  T_contract   :: Type -> T
-  T_pair       :: Type -> Type -> T
-  T_or         :: Type -> Type -> T
-  T_lambda     :: Type -> Type -> T
-  T_map        :: Comparable -> Type -> T
-  T_big_map    :: Comparable -> Type -> T
+data Op =
+    PRIM I
+  | MAC Macro
+  | SEQ Ops
   deriving Show
 
--- comparable type
-data Comparable = Comparable CT TypeNote deriving Show
-
--- instance Show Comparable where
---   show (Comparable t tn) = (show t) ++ (show tn)
-
-data CT where
-  T_int       :: CT
-  T_nat       :: CT
-  T_string    :: CT
-  T_bytes     :: CT
-  T_mutez     :: CT
-  T_bool      :: CT
-  T_key_hash  :: CT
-  T_timestamp :: CT
-  T_address   :: CT
+data I =
+    DROP
+  | DUP VarNote
+  | SWAP
+  | PUSH VarNote Type Data
+  | SOME TypeNote VarNote FieldNote
+  | NONE TypeNote VarNote FieldNote Type
+  | UNIT TypeNote VarNote
+  | IF_NONE Ops Ops
+  | PAIR              TypeNote VarNote FieldNote FieldNote
+  | CAR               VarNote FieldNote
+  | CDR               VarNote FieldNote
+  | LEFT              TypeNote VarNote FieldNote FieldNote Type
+  | RIGHT             TypeNote VarNote FieldNote FieldNote Type
+  | IF_LEFT           Ops Ops
+  | IF_RIGHT          Ops Ops
+  | NIL               TypeNote VarNote Type
+  | CONS              VarNote
+  | IF_CONS           Ops Ops
+  | SIZE              VarNote
+  | EMPTY_SET         TypeNote VarNote Comparable
+  | EMPTY_MAP         TypeNote VarNote Comparable Type
+  | MAP               VarNote Ops
+  | ITER              VarNote Ops
+  | MEM               VarNote
+  | GET               VarNote
+  | UPDATE
+  | IF                Ops Ops
+  | LOOP              Ops
+  | LOOP_LEFT         Ops
+  | LAMBDA            VarNote Type Type Ops
+  | EXEC              VarNote
+  | DIP               Ops
+  | FAILWITH
+  | CAST              TypeNote VarNote
+  | RENAME            VarNote
+  | PACK              VarNote
+  | UNPACK            VarNote Type
+  | CONCAT            VarNote
+  | SLICE             VarNote
+  | ISNAT
+  | ADD               VarNote
+  | SUB               VarNote
+  | MUL               VarNote
+  | EDIV              VarNote
+  | ABS               VarNote
+  | NEG
+  | MOD
+  | LSL               VarNote
+  | LSR               VarNote
+  | OR                VarNote
+  | AND               VarNote
+  | XOR               VarNote
+  | NOT               VarNote
+  | COMPARE           VarNote
+  | EQ                VarNote
+  | NEQ               VarNote
+  | LT                VarNote
+  | GT                VarNote
+  | LE                VarNote
+  | GE                VarNote
+  | INT               VarNote
+  | SELF              VarNote
+  | CONTRACT          Type
+  | TRANSFER_TOKENS   VarNote
+  | SET_DELEGATE
+  | CREATE_ACCOUNT    VarNote VarNote
+  | CREATE_CONTRACT   VarNote VarNote
+  | CREATE_CONTRACT2  VarNote VarNote Contract
+  | IMPLICIT_ACCOUNT  VarNote
+  | NOW               VarNote
+  | AMOUNT            VarNote
+  | BALANCE           VarNote
+  | CHECK_SIGNATURE   VarNote
+  | SHA256            VarNote
+  | SHA512            VarNote
+  | BLAKE2B           VarNote
+  | HASH_KEY          VarNote
+  | STEPS_TO_QUOTA    VarNote
+  | SOURCE            VarNote
+  | SENDER            VarNote
+  | ADDRESS           VarNote
   deriving Show
-
--- Note type
-
---instance Show TypeNote where
---  show (TypeNote Nothing) = ""
---  show (TypeNote (Just tn)) = ':' : (T.unpack tn)
---
---instance Show FieldNote where
---  show (FieldNote Nothing) = ""
---  show (FieldNote (Just fn)) = '%' : (T.unpack fn)
---
---instance Show VarNote where
---  show (VarNote Nothing) = ""
---  show (VarNote (Just vn)) = '@' : (T.unpack vn)
-
-noTN = TypeNote Nothing
-noFN = FieldNote Nothing
-noVN = VarNote Nothing
 
