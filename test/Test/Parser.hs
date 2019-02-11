1b003a0c6d5eca1b6b5778274bb982ca2b911970
rvem <roman.melnikov@serokell.io>
2019-02-11 16:24:41 +0300

    [TM-15] Fix `value` parser
    Problem: `value` parser fails on lambda parsing. It happens because
    value parsers for list and map also start with `symbol '{'`.
    
    Solution: add `try` to list and map value parsers so they will backtrack
    if they fail.

--- test/Test/Parser.hs
+++ test/Test/Parser.hs
@@ -3,15 +3,17 @@ module Test.Parser
   ) where
 
 import Data.List (isSuffixOf)
-import Morley.Parser (contract)
+import Morley.Parser (contract, value)
+import Morley.Types as M
 import System.Directory (listDirectory)
-import Test.Hspec (Expectation, Spec, describe, it, shouldSatisfy)
+import Test.Hspec (Expectation, Spec, describe, it, shouldBe, shouldSatisfy)
 import Text.Megaparsec (parse)
 
 
 spec :: Spec
 spec = describe "Parser tests" $ do
   it "Successfully parses contracts examples from contracts/" parseContractsTest
+  it "value parser test" valueParserTest
 
 parseContractsTest :: Expectation
 parseContractsTest = do
@@ -24,3 +26,12 @@ checkFile :: FilePath -> Expectation
 checkFile file = do
   code <- readFile file
   parse contract file code `shouldSatisfy` isRight
+
+valueParserTest :: Expectation
+valueParserTest = do
+  parse value "" "{PUSH int 5;}" `shouldBe`
+    (Right $ M.ValueLambda [M.PRIM (M.PUSH Nothing (M.Type (M.T_comparable M.T_int) Nothing) (M.ValueInt 5))])
+  parse value "" "{1; 2}" `shouldBe`
+    (Right $ M.ValueSeq [M.ValueInt 1, M.ValueInt 2])
+  parse value "" "{Elt 1 2; Elt 3 4}" `shouldBe`
+    (Right $ M.ValueMap [M.Elt (M.ValueInt 1) (M.ValueInt 2), M.Elt (M.ValueInt 3) (M.ValueInt 4)])
