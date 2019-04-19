8703c9a963da272eccf62377a02168eed71e8e07
Ivan Gromakovskii <gie1994@gmail.com>
2019-04-19 22:37:38 +0300

    [TM-44] Cleanup Michelson.Parser after refactoring
    1. Sort out exports: put into proper sections, remove useless ones.
    2. Remove useless `hiding` from imports, avoid using Michelson.Types
    where possible, because such module shouldn't exist, it's a leftover.
    3. Move definitions which are used in only one function into `where`
    blocks to make it clearer that they are used only there.
    4. Rename `op'` to `parsedOp` because it sounds more descriptive and
    better corresponds to the existing naming scheme.

--- src/Michelson/Parser.hs
+++ src/Michelson/Parser.hs
@@ -4,10 +4,7 @@ module Michelson.Parser
 
   -- * Parsers
   , program
-  , codeEntry
-  , type_
   , value
-  , stackType
 
   -- * Errors
   , CustomParserException (..)
@@ -18,13 +15,15 @@ module Michelson.Parser
   , parseNoEnv
 
   -- * For tests
+  , codeEntry
+  , type_
   , stringLiteral
   , bytesLiteral
   , intLiteral
   , printComment
   ) where
 
-import Prelude hiding (many, note, try)
+import Prelude hiding (try)
 
 import Control.Applicative.Permutations (intercalateEffect, toPermutation)
 import Text.Megaparsec (Parsec, choice, eitherP, parse, try)
@@ -40,24 +39,11 @@ import Michelson.Parser.Types
 import Michelson.Parser.Value
 import Michelson.Types (ParsedOp(..))
 import qualified Michelson.Types as Mi
+import Michelson.Untyped
 
--------------------------------------------------------------------------------
--- Top-Level Parsers
--------------------------------------------------------------------------------
-
-
--- Contracts
-------------------
-
--- | Michelson contract with let definitions
-program :: Parsec CustomParserException Text (Mi.Contract' ParsedOp)
-program = runReaderT programInner noLetEnv
-
-programInner :: Parser (Mi.Contract' ParsedOp)
-programInner = do
-  mSpace
-  env <- fromMaybe noLetEnv <$> (optional (letBlock op'))
-  local (const env) contract
+----------------------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------------------
 
 -- | Parse with empty environment
 parseNoEnv ::
@@ -66,6 +52,23 @@ parseNoEnv ::
   -> Text
   -> Either (ParseErrorBundle Text CustomParserException) a
 parseNoEnv p = parse (runReaderT p noLetEnv)
+
+-------------------------------------------------------------------------------
+-- Parsers
+-------------------------------------------------------------------------------
+
+-- Contract
+------------------
+
+-- | Michelson contract with let definitions
+program :: Parsec CustomParserException Text (Contract' ParsedOp)
+program = runReaderT programInner noLetEnv
+  where
+    programInner :: Parser (Contract' ParsedOp)
+    programInner = do
+      mSpace
+      env <- fromMaybe noLetEnv <$> (optional (letBlock parsedOp))
+      local (const env) contract
 
 -- | Michelson contract
 contract :: Parser (Mi.Contract' ParsedOp)
@@ -76,58 +79,59 @@ contract = do
                    <*> toPermutation storage
                    <*> toPermutation code
   return $ Mi.Contract p s c
+  where
+    parameter :: Parser Type
+    parameter = symbol "parameter" *> type_
+
+    storage :: Parser Type
+    storage = symbol "storage" *> type_
+
+    code :: Parser [ParsedOp]
+    code = symbol "code" *> codeEntry
+
 
 -- Value
 ------------------
 
 value :: Parser Mi.ParsedValue
-value = value' op'
+value = value' parsedOp
 
 
 -- Primitive instruction
 ------------------
 
 prim :: Parser Mi.ParsedInstr
-prim = primInstr contract op'
+prim = primInstr contract parsedOp
 
--- Contract Blocks
+-- Parsed operations (primitive instructions, macros, extras, etc.)
 ------------------
 
-parameter :: Parser Mi.Type
-parameter = do void $ symbol "parameter"; type_
-
-storage :: Parser Mi.Type
-storage = do void $ symbol "storage"; type_
-
-code :: Parser [ParsedOp]
-code = do void $ symbol "code"; codeEntry
-
--- Michelson expressions
-------------------------
 -- | Parses code block after "code" keyword of a contract.
 --
 -- This function is part of the module API, its semantics should not change.
 codeEntry :: Parser [ParsedOp]
 codeEntry = ops
 
-op' :: Parser Mi.ParsedOp
-op' = do
+parsedOp :: Parser Mi.ParsedOp
+parsedOp = do
   lms <- asks letMacros
   choice
     [ (Mi.Prim . Mi.EXT) <$> extInstr ops
     , Mi.LMac <$> mkLetMac lms
     , Mi.Prim <$> prim
-    , Mi.Mac <$> macro op'
+    , Mi.Mac <$> macro parsedOp
     , primOrMac
     , Mi.Seq <$> ops
     ]
 
 ops :: Parser [Mi.ParsedOp]
-ops = ops' op'
+ops = ops' parsedOp
 
 -------------------------------------------------------------------------------
--- Macro Parsers
+-- Mixed parsers
+-- These are needed for better error messages
 -------------------------------------------------------------------------------
+
 ifOrIfX :: Parser Mi.ParsedOp
 ifOrIfX = do
   symbol' "IF"
@@ -139,6 +143,6 @@ ifOrIfX = do
 -- Some of the operations and macros have the same prefixes in their names
 -- So this case should be handled separately
 primOrMac :: Parser Mi.ParsedOp
-primOrMac = ((Mi.Mac <$> ifCmpMac op') <|> ifOrIfX)
-  <|> ((Mi.Mac <$> mapCadrMac op') <|> (Mi.Prim <$> mapOp op'))
+primOrMac = ((Mi.Mac <$> ifCmpMac parsedOp) <|> ifOrIfX)
+  <|> ((Mi.Mac <$> mapCadrMac parsedOp) <|> (Mi.Prim <$> mapOp parsedOp))
   <|> (try (Mi.Prim <$> pairOp) <|> Mi.Mac <$> pairMac)
