2aeec4624fef79573c45442ed6944f4b0a4aba74
martoon <martoon.hsk@gmail.com>
2019-05-22 16:09:28 +0300

    [TM-181] Parse modules manually for discovery
    Problem: using `haskell-src-exts` package is considered not a good
    practice, and in fact it does not support all Haskell extentions we'd
    probably like to use some day, e.g. `BlockArguments`.
    
    Also it is very choosy to haskell modules, and taking into account
    that contracts-discovery is called before actual compilation of
    *all modules* in `lorentz-contracts` (cabal scans import lists first
    to build module dependency tree), minor mistakes in any of the modules
    make discovery crash.
    
    Solution: parse module export list with manually written parser.
    
    If after this point discovery continues producing problems, I think
    we'd better switch to manual contracts registry maintenance.

--- src/Lorentz/Discover.hs
+++ src/Lorentz/Discover.hs
@@ -3,9 +3,20 @@
 -- All the discovery logic resides in 'lorentz-discover' executable.
 module Lorentz.Discover
   ( IsContract (..)
+
+  , ExportedContractInfo (..)
+  , ExportedContractDecl (..)
+
+  , haskellExportsParser
   ) where
 
+import Data.Char (isAlphaNum)
 import Data.Singletons (SingI)
+import qualified Data.Text as T
+import Text.Megaparsec (Parsec)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as PL
 
 import qualified Lorentz.Base as L
 import Lorentz.Constraints
@@ -27,3 +38,71 @@ instance ( SingI (T.ToT cp), SingI (T.ToT st)
          ) =>
          IsContract (L.Contract cp st) where
   toUntypedContract = toUntypedContract . L.compileLorentzContract
+
+-- | Information about a contract required for contracts registry.
+data ExportedContractInfo = ExportedContractInfo
+  { eciModuleName :: Text
+  , eciContractDecl :: ExportedContractDecl
+  } deriving (Show, Eq)
+
+-- | Contract names, for Haskell and for humans.
+data ExportedContractDecl = ExportedContractDecl
+  { ecdName :: Text
+    -- ^ Identifier of a contract, e.g. "auction".
+  , ecdVar :: Text
+    -- ^ Name of a contract as is appears in Haskell code.
+  } deriving (Show, Eq)
+
+haskellExportsParser :: Parsec Void Text [ExportedContractInfo]
+haskellExportsParser = do
+  space
+  symbol "module"
+  moduleName <-
+    lexeme $ P.takeWhile1P (Just "module name")
+                           (\c -> isAlphaNum c || c `elem` ['.', '_'])
+  symbol "("
+  exports <- exportItems
+  symbol ")"
+  symbol "where"
+  return
+    [ ExportedContractInfo
+      { eciModuleName = moduleName
+      , eciContractDecl = decl
+      }
+    | exportRaw <- exports
+    , let export = T.strip exportRaw
+    , Just decl <- pure $ toContractDecl export
+    ]
+  where
+    exportItems :: Parsec Void Text [Text]
+    exportItems = P.sepBy exportItem (symbol ",")
+
+    -- We do not follow the syntax precisely, just trying to parse
+    -- all valid Haskell programs
+    exportItem = fmap mconcat . many . lexeme $
+      P.choice
+        [ P.takeWhile1P Nothing isExportEntryChar
+        , do symbol "("
+             _ <- exportItems
+             symbol ")"
+             return "(..)"  -- we are not interested in content
+        , symbol ".." $> ".."
+        ]
+
+    space = PL.space P.space1 (PL.skipLineComment "--")
+                              (PL.skipBlockComment "{-" "-}")
+    lexeme = PL.lexeme space
+    symbol = void . PL.symbol space
+
+isExportEntryChar :: Char -> Bool
+isExportEntryChar c = isAlphaNum c || c == '_'
+
+toContractDecl :: Text -> Maybe ExportedContractDecl
+toContractDecl varName = do
+  rawName <- T.stripPrefix "contract_" varName
+  guard $ all isExportEntryChar $ toString rawName
+  let name = T.replace "_" " " rawName
+  return ExportedContractDecl
+    { ecdName = name
+    , ecdVar = varName
+    }
